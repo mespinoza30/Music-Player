950a96e8ca640c8d13f511f940d0c4adbe1bc87d
mespinoza30 <m.espinoza16@hotmail.com>
2021-06-07 22:00:52 -0700

    progress bar is clickable

--- script.js
+++ script.js
@@ -73,6 +73,14 @@ function updateProgress(e) {
     progress.style.width = `${progressPercent}%`;
 }
 
+function setProgress(e) {
+    const width = this.clientWidth
+    const clickX = e.offsetX
+    const duration = audio.duration
+
+    audio.currentTime = (clickX / width) * duration
+}
+
 //Event listeners
 playBtn.addEventListener('click', () => {
     const isPlaying = musicContainer.classList.contains('play');
@@ -90,3 +98,5 @@ prevBtn.addEventListener('click', prevSong)
 nextBtn.addEventListener('click', nextSong)
 
 audio.addEventListener('timeupdate', updateProgress)
+
+progressContainer.addEventListener('click', setProgress)
